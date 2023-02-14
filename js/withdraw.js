193633143ffe464b3abeab31ca84c1c383de2eb8
SQLAdmin <abuyeahia24@gmail.com>
2023-02-14 11:28:32 +0600

    again

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,7 +1,17 @@
 
 // 1. add withdraw button event handler 
 // 2. get withdraw amount by using getInputValueById function
+// 3. get previous withdraw amount by using getTextElementValueById function
+// 4. calculate new withdraw Total and set the value
+// 4-5: set new withdraw total by using setTextElementValueById function
 
 document.getElementById('btn-withdraw').addEventListener('click', function(){
     // console.log("bal click")
+
+    const newWithdrawAmount = getInputFieldValueById('withdraw-field');
+    // console.log(newWithdrawAmount);
+    const previousWithdrawTotal = getTextElementValueById('withdraw-total');
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+
+
 })
