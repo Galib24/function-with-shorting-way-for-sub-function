fd1649f1c7ac0b2ea5cda0eb32c33bc5a03f499f
SQLAdmin <abuyeahia24@gmail.com>
2023-02-14 10:43:41 +0600

    added some operation of function

--- js/deposit.js
+++ js/deposit.js
@@ -2,11 +2,20 @@ function getInputFieldValueById(inputFieldId){
     const inputField = document.getElementById(inputFieldId);
     const inputFieldValueString = inputField.value;
     const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
     return inputFieldValue;
+}
+
+function getElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueString = element.innerText;
+    const value = parseFloat(elementValueString);
+    return value;
 }
 
 document.getElementById('btn-deposit').addEventListener('click', function(){
     // 1. get element by id
     // 2. get the value from the element
     // 3.convert string value to a number
+    const newDepositAmount = getInputFieldValueById('deposit-field');
 })
