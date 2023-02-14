4af73e9aed9b0218553c22d49b92a0d92f9e7bd0
SQLAdmin <abuyeahia24@gmail.com>
2023-02-14 11:46:47 +0600

    added again operation

--- js/withdraw.js
+++ js/withdraw.js
@@ -4,6 +4,9 @@
 // 3. get previous withdraw amount by using getTextElementValueById function
 // 4. calculate new withdraw Total and set the value
 // 4-5: set new withdraw total by using setTextElementValueById function
+// 5. get previous balance total by using setTextElementValueById function
+// 6. calculate new balance total
+// 7.set balance total using setTextElementValueById
 
 document.getElementById('btn-withdraw').addEventListener('click', function(){
     // console.log("bal click")
@@ -12,6 +15,9 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     // console.log(newWithdrawAmount);
     const previousWithdrawTotal = getTextElementValueById('withdraw-total');
     const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-
+    setTextElementValueById('withdraw-total', newWithdrawTotal);
+    const previousBalanceTotal = getTextElementValueById('balance-total');
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    setTextElementValueById('balance-total', newBalanceTotal);
 
 })
